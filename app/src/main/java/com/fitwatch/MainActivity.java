0bc622868ac8d94e82dbbb2a2770243aef2ebc4b
Ashish0077 <ashish007722@gmail.com>
2020-09-14 08:15:48 +0530

    Splash Animations
    Signed-off-by: Ashish0077 <ashish007722@gmail.com>

--- app/src/main/java/com/fitwatch/MainActivity.java
+++ app/src/main/java/com/fitwatch/MainActivity.java
@@ -4,23 +4,38 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView tvSplash, tvSubSplash;
     Button btnget;
+    ImageView ivSplash;
+    Animation atg, btgOne, btgTwo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ivSplash = findViewById(R.id.ivSplash);
         tvSplash = findViewById(R.id.tvSplash);
         tvSubSplash = findViewById(R.id.tvSubSplash);
         btnget = findViewById(R.id.btnget);
 
+        atg = AnimationUtils.loadAnimation(this, R.anim.atg);
+        btgOne = AnimationUtils.loadAnimation(this, R.anim.btgone);
+        btgTwo = AnimationUtils.loadAnimation(this, R.anim.btgtwo);
+
+        // passing time
+        ivSplash.startAnimation(atg);
+        tvSplash.startAnimation(btgOne);
+        tvSubSplash.startAnimation(btgOne);
+        btnget.startAnimation(btgTwo);
         // import font
         Typeface MLight = Typeface.createFromAsset(getAssets(), "fonts/MLight.ttf");
         Typeface MMedium = Typeface.createFromAsset(getAssets(), "fonts/MMedium.ttf");
