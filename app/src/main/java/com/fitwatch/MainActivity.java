5405f6389459bdba571395e2280b3860eb86dd4f
Ashish0077 <ashish007722@gmail.com>
2020-09-14 07:56:00 +0530

    Initial Commit added Splash Screen
    Signed-off-by: Ashish0077 <ashish007722@gmail.com>

---
+++ app/src/main/java/com/fitwatch/MainActivity.java
@@ -0,0 +1,34 @@
+package com.fitwatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvSplash, tvSubSplash;
+    Button btnget;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvSplash = findViewById(R.id.tvSplash);
+        tvSubSplash = findViewById(R.id.tvSubSplash);
+        btnget = findViewById(R.id.btnget);
+
+        // import font
+        Typeface MLight = Typeface.createFromAsset(getAssets(), "fonts/MLight.ttf");
+        Typeface MMedium = Typeface.createFromAsset(getAssets(), "fonts/MMedium.ttf");
+        Typeface MRegular = Typeface.createFromAsset(getAssets(), "fonts/MRegular.ttf");
+
+        // customize font
+        tvSplash.setTypeface(MRegular);
+        tvSubSplash.setTypeface(MLight);
+        btnget.setTypeface(MMedium);
+    }
+}
